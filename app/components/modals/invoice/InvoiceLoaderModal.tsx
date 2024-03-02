90a9784ca864b4ac679b7da147edf04e8443d8a4
Jason Sun <sunapi386@gmail.com>
2024-03-01 21:26:29 -0500

    Add VS Code settings and update BaseNavbar component

--- app/components/modals/invoice/InvoiceLoaderModal.tsx
+++ app/components/modals/invoice/InvoiceLoaderModal.tsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 
 // ShadCn
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 
 // Components
@@ -17,91 +17,73 @@ import { SavedInvoicesList } from "@/app/components";
 
 // Context
 import { useInvoiceContext } from "@/contexts/InvoiceContext";
-import { ToastContainer, toast } from 'react-toastify';
+import { useFormContext } from "react-hook-form";
+import { toast } from "react-toastify";
 
 type InvoiceLoaderModalType = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const InvoiceLoaderModal = ({ children }: InvoiceLoaderModalType) => {
-    const [open, setOpen] = useState(false);
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const [uploadProgress, setUploadProgress] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const { reset } = useFormContext();
 
-    const { savedInvoices } = useInvoiceContext();
+  const { savedInvoices } = useInvoiceContext();
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files && files.length > 0) {
-            setSelectedFile(files[0]);
-        }
-    };
-
-    function handleUpload() {
-        // Process the selected file JSON here
-         if (selectedFile && selectedFile.type === 'application/json') {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const jsonData = event.target?.result;
-                // Your JSON processing logic here
-                console.log("JSON Data:", jsonData);
-            };
-            reader.readAsText(selectedFile);
-        } else {
-            console.log("Unsupported file type");
-        }
-        // Toast the upload status
-        if (selectedFile) {
-            // Show a toast message indicating the upload status
-            const toastMessage = `Uploading ${selectedFile.name}...`;
-            toast(toastMessage);
-
-            // Simulate upload progress
-            let progress = 0;
-            const interval = setInterval(() => {
-                progress += 10;
-                setUploadProgress(progress);
-                if (progress >= 100) {
-                    clearInterval(interval);
-                    // Show a toast message indicating the upload is complete
-                    const toastMessage = `${selectedFile.name} uploaded successfully!`;
-                    toast(toastMessage);
-                }
-            }, 500);
-        }
-        // Close the modal after processing
-        setOpen(false);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]);
     }
+  };
 
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>{children}</DialogTrigger>
+  function handleUpload() {
+    // Process the selected file JSON here
+    if (selectedFile && selectedFile.type === "application/json") {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const jsonData = event.target?.result;
+        // Your JSON processing logic here
+        console.log("JSON Data:", jsonData);
+        try {
+          const parsedData = JSON.parse(jsonData as string);
+          reset(parsedData);
+          toast.success("Data loaded successfully");
+        } catch (error) {
+          toast.error("Error parsing JSON data");
+          console.log("Error parsing JSON data:", error);
+        }
+      };
+      reader.readAsText(selectedFile);
+    } else {
+      console.log("Unsupported file type");
+    }
+    setSelectedFile(null);
+    setOpen(false);
+  }
 
-            <DialogContent>
-                <DialogHeader className="pb-2 border-b">
-                    <DialogTitle>Saved Invoices</DialogTitle>
-                    <DialogDescription>
-                        You have {savedInvoices.length} saved invoices
-                    </DialogDescription>
-                </DialogHeader>
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
 
+      <DialogContent>
+        <DialogHeader className="pb-2 border-b">
+          <DialogTitle>Saved Invoices</DialogTitle>
+          <DialogDescription>
+            You have {savedInvoices.length} saved invoices
+          </DialogDescription>
+        </DialogHeader>
 
-                <div>
-                    <input type="file" onChange={handleFileChange} accept=".json" />
-                    <button onClick={handleUpload}>Upload</button>
-                    {selectedFile && (
-                        <div>
-                            <progress value={uploadProgress} max={100} />
-                            <span>{uploadProgress}%</span>
-                        </div>
-                    )}
-                </div>
+        <div>
+          <input type="file" onChange={handleFileChange} accept=".json" />
+          <button onClick={handleUpload}>Load</button>
+        </div>
 
-                <SavedInvoicesList setModalState={setOpen} />
-            </DialogContent>
-        </Dialog>
-    );
-
+        <SavedInvoicesList setModalState={setOpen} />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default InvoiceLoaderModal;
